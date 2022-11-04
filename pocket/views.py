057368d902528e8351555a6db45b048d694225cc
laagom <cjyooong@gmail.com>
2022-11-04 22:19:26 +0900

    FEAT : MODIFY urlpatterns AND template_name 1. urls.py와 views.py의 template_name을 수정하여 base.html로 이동하는 경로 변경

--- pocket/views.py
+++ pocket/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-class FooterView(TemplateView):
-    template_name = "footer.html"
+class BaseView(TemplateView):
+    template_name = "base.html"
 
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
