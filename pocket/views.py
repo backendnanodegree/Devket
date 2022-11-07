0393cb3a7bd87c495f703902b5d0d911f9f1f5a8
jeha00 <rudtls0611@naver.com>
2022-11-08 00:18:12 +0900

    Add views, urls for payment

--- pocket/views.py
+++ pocket/views.py
@@ -24,3 +24,20 @@ class PwdHelpView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
+
+
+class PremiumView(TemplateView):
+    template_name = 'premium/base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['login'] = True
+        return context
+        # return render(request, 'premium/base.html', {'login': login})
+
+
+class PaymentView(TemplateView):
+    template_name = 'premium/payment.html'
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
