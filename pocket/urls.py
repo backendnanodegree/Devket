4affd0d9d2885e4f03c598f49452fca2b669c280
jeha00 <rudtls0611@naver.com>
2022-11-14 13:15:32 +0900

    FEAT: Add api_url, view_url, View class - add api_url, view_url for mylist - add MyListView, ListAPIView class for mylist

--- pocket/urls.py
+++ pocket/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from pocket.views import (
     HomeView, 
@@ -8,17 +8,30 @@ from pocket.views import (
     PwdHelpView, 
     PremiumView,
     PaymentView,
+    MyListView,
+    ListAPIView,
 )
+
+
+api_patterns = [
+    path('list/', ListAPIView.as_view()),
+]
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    
+
     # user
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LogInView.as_view(), name='login'),
     path('login/password/', PwdHelpView.as_view(), name='password'),
-    
+
+    #mylist
+    path('mylist/', MyListView.as_view(), name='mylist'), 
+
     # payment
     path('premium/', PremiumView.as_view(), name='premium'),
     path('premium/payment/', PaymentView.as_view(), name='payment'),
+
+    # api
+    path('api/', include(api_patterns)),
 ]
