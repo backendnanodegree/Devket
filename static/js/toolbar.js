bc6f8efd5c9e8fcf0b2080d3af8ea45a290e6ea0
laagom <cjyooong@gmail.com>
2022-11-24 03:20:39 +0900

    FIX: fix bulk favorite function

--- static/js/toolbar.js
+++ static/js/toolbar.js
@@ -235,7 +235,7 @@ function makeBulkTopToolBar() {
     favoriteIcon.insertAdjacentHTML('beforeend', favoriteIconSvgHtml)
     const favoriteIconSvg               = getElement('.favorite-icon-svg')
 
-    const favoriteIconPathHtml          = `<path fill-rule="evenodd" clip-rule="evenodd" d="M12 1a1 1 0 0 1 .897.557l2.706 5.484 6.051.88a1 1 0 0 1 .555 1.705l-4.38 4.268 1.034 6.027a1 1 0 0 1-1.45 1.054L12 18.13l-5.413 2.845a1 1 0 0 1-1.45-1.054l1.033-6.027-4.379-4.268a1 1 0 0 1 .555-1.706l6.051-.88 2.706-5.483A1 1 0 0 1 12 1Zm0 3.26L9.958 8.397a1 1 0 0 1-.753.548l-4.567.663 3.305 3.221a1 1 0 0 1 .287.885l-.78 4.548 4.085-2.147a1 1 0 0 1 .93 0l4.085 2.147-.78-4.548a1 1 0 0 1 .287-.885l3.305-3.22-4.567-.664a1 1 0 0 1-.753-.548L12 4.26Z"></path>`
+    const favoriteIconPathHtml          = `<path fill-rule="evenodd" clip-rule="evenodd" d="M12 1a1 1 0 0 1 .897.557l2.706 5.484 6.051.88a1 1 0 0 1 .555 1.705l-4.38 4.268 1.034 6.027a1 1 0 0 1-1.45 1.054L12 18.13l-5.413 2.845a1 1 0 0 1-1.45-1.054l1.033-6.027-4.379-4.268a1 1 0 0 1 .555-1.706l6.051-.88 2.706-5.483A1 1 0 0 1 12 1Zm0 3.26L9.958 8.397a1 1 0 0 1-.753.548l-4.567.663 3.305 3.221a1 1 0 0 1 .287.885l-.78 4.548 4.085-2.147a1 1 0 0 1 .93 0l4.085 2.147-.78-4.548a1 1 0 0 1 .287-.885l3.305-3.22-4.567-.664a1 1 0 0 1-.753-.548L12 4.26Z" class='true'></path>`
     favoriteIconSvg.insertAdjacentHTML('beforeend', favoriteIconPathHtml)
 
     const categoryButton                = createNode('button')
@@ -488,22 +488,32 @@ function bulkButtonEventSet() {
 function favoriteBulkSelectedSite() {
     /* bulk 즐겨찾기 이벤트 */
 
+    let favorite = JSON.parse(getElement('.favorite-icon-svg>path').className['baseVal'])
+    let alamText = favorite ? '선택한 항목을 즐겨찾기목록에 추가하시겠습니까?' :'선택한 항목을 즐겨찾기 목록에서 제거하시겠습니까?'
+
     if (selected_articles.length > 0) {
-        if(confirm('선택한 항목을 즐겨찾기목록에 추가하시겠습니까?')) {
+        if(confirm(alamText)) {
             const data = setFechData("PUT",{
                 pk_ids: selected_articles,//[1, 2, 3, 4]
-                favorite: true,
+                favorite: favorite,
                 user: "User Id"
             })
     
             fetch(`/api/sites/bulk`, data)
                 .then(response => {
                     let status = response.status
-    
-                    if(status == 200) {
-                        alert('즐겨찾기 목록에 추가하였습니다.')
-                    }else if(status == 400) {
-                        alert('즐겨찾기 목록에 추가하지 못했습니다.')
+                    if(favorite){
+                        if(status == 200) {
+                            alert('즐겨찾기 목록에 추가하였습니다.')
+                        }else if(status == 400) {
+                            alert('즐겨찾기 목록에 추가하지 못했습니다.')
+                        }
+                    }else{
+                        if(status == 200) {
+                            alert('즐겨찾기 목록에서 제거했습니다.')
+                        }else if(status == 400) {
+                            alert('즐겨찾기 목록에서 제거하지 못했습니다.')
+                        }
                     }
                 })
                 .then(() => getSiteList())
