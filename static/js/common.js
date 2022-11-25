e4f622a17cbb1706e699fe93f52429be644adc63
laagom <cjyooong@gmail.com>
2022-11-25 12:18:40 +0900

    ADD: add common function that insert element after reference element
    1. javascript에 참조가 되는 요소 뒤에 요소를 추가하는 함수가 없어 추가

--- static/js/common.js
+++ static/js/common.js
@@ -49,6 +49,12 @@ function appendTag(parent, element) {
     return parent.appendChild(element);
 }
 
+function insertAfter(element, referenceElement) {
+    /* 요소 다음에 추가하는 함수 */
+
+    referenceElement.parentElement.insertBefore(element, referenceElement.nextSibling);
+}
+
 function removeAllNode(element) {
     /* 요소의 내부 요소 초기화 */
 
@@ -102,6 +108,7 @@ export {
     removeElement,
     createNode, 
     appendTag,
+    insertAfter,
     removeAllNode,
     getCookie, 
     setFechData,
