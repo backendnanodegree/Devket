844c2b164395f8042cb2e9c940c219c2f8e1eeb2
jeha00 <rudtls0611@naver.com>
2022-12-01 11:56:47 +0900

    FEAT: delete attributes of User model - delete card_number, cvc, expiration_data attribute

--- pocket/models.py
+++ pocket/models.py
@@ -11,8 +11,8 @@ class User(models.Model):
     name                        = models.CharField(verbose_name='이름', max_length = 20)
     password                    = models.CharField(verbose_name='비밀번호', max_length = 15)
     introduce                   = models.TextField(verbose_name='자기소개', max_length = 200)
-    profile_picture             = models.ImageField(verbose_name='프로필사진', null=True, upload_to=f"profile/", blank=True)
     blog_url                    = models.CharField(verbose_name='블로그url', max_length = 250)
+    profile_picture             = models.ImageField(verbose_name='프로필사진', null=True, upload_to=f"profile/", blank=True)
 
     # Payment_status choices
     PAYMENT_ON                  = 1
@@ -24,9 +24,6 @@ class User(models.Model):
                                   ]
     
     payment_status              = models.IntegerField(choices=PAYMENT_CHOICES, default=PAYMENT_OFF, verbose_name='결제상태')
-    card_number                 = models.IntegerField(verbose_name='카드번호')
-    cvc                         = models.IntegerField(verbose_name='cvc')
-    expiration_date             = models.IntegerField(verbose_name='카드유효기간')
     created_at                  = models.DateTimeField(verbose_name='생성일', auto_now_add=True)
     updated_at                  = models.DateTimeField(verbose_name='갱신일', auto_now=True)
 
